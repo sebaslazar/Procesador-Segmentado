39f0a3dc0a4bc607e511e91962a82ec490843dc5
sebaslazar <sebastian.cacante@utp.edu.co>
2024-05-25 10:56:50 -0500

    Add Font ROM

--- Sync_module.sv
+++ Sync_module.sv
@@ -15,20 +15,22 @@ V Back Porch = 38 Lines
 V Sync = 3 Lines
 */
 
-module VGA_module (
+module Sync_module (
     input logic clk,
-    input logic [23:0] Pixel,
-    output logic [7:0] VGA_red,
-    output logic [7:0] VGA_green,
-    output logic [7:0] VGA_blue,
-    output logic VGA_clk,
     output logic VGA_sync,
     output logic VGA_blank,
     output logic hsync,
-    output logic vsync
+    output logic vsync,
+    output logic H,
+    output logic V
 );
-    logic [10:0] H_pos = 11'b0;
-    logic [10:0] V_pos = 11'b0;
+    logic [10:0] H_pos;
+    logic [10:0] V_pos;
+
+    initial begin
+        H_pos = 11'b0;
+        V_pos = 11'b0;
+    end
     
     always @(posedge clk) begin
         if (H_pos < 1688) begin //Recorrido por toda la pantalla
@@ -51,26 +53,21 @@ module VGA_module (
         end
 
         if ((V_pos > 1024) && (V_pos < 1028)) begin //Sincronización Vertical (Revisar)
-            V_pos <= 1'b0;
+            vsync <= 1'b0;
             VGA_sync <= 1'b0;
         end else begin
-            V_pos <= 1'b1;
+            vsync <= 1'b1;
             VGA_sync <= 1'b1;
         end
 
         if (((H_pos > 1279) && (H_pos < 1688)) || ((V_pos > 1023) && (V_pos < 1066))) begin //Desactiva RGB
-            VGA_red <= 8'b0;
-            VGA_green <= 8'b0;
-            VGA_blue <= 8'b0;
             VGA_blank <= 1'b0;
         end else begin
-            VGA_red <= Pixel[23:16];
-            VGA_green <= Pixel[15:8];
-            VGA_blue <= Pixel[7:0];
             VGA_blank <= 1'b1;
         end
     end
 
-    assign VGA_clk = clk; //The CLOCK input of the ADV7123 is typically the pixel clock rate of the system
+    assign H = H_pos;
+    assign V = V_pos;
 
 endmodule
