0e57f969b821f34920250d4b1a0a8a221d033b89
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2024-05-26 23:06:02 -0500

    General Update

--- Testbenches/Testbench_ScreenRAM.sv
+++ Testbenches/Testbench_ScreenRAM.sv
@@ -1,29 +1,31 @@
 module tb_ScreenRAM;
     logic clk;
+    logic [13:0] address;
+    logic [7:0] char;
+
     logic [7:0] scrX;
-    logic [6:0] scrY;
-    logic selector;
-    logic [7:0] caracter;
+    logic [5:0] scrY;
+    integer pixel_counter;
+    integer line_counter;
 
     Screen_RAM Screen_RAM_Test(
         .clk(clk),
-        .scrX(scrX),
-        .scrY(scrY),
-        .selector(selector),
-        .caracter(caracter)
+        .address({scrY, scrX}),
+        .char(char)
     );
     
     initial
     begin
         clk = 1'b0;
         scrX = 8'b0;
-        scrY = 7'b0;
-        selector = 1'b1;
+        scrY = -6'b1;
+        pixel_counter = 0;
+        line_counter = 0;
 
         $dumpfile("dump.vcd");
         $dumpvars;
 
-        #500;
+        #10560;
         $finish;
     end
 
@@ -32,13 +34,23 @@ module tb_ScreenRAM;
     end
 
     always @(posedge clk) begin
-        if (scrX <= 8'd159) begin
-            scrX = scrX + 1;
+        if (pixel_counter%160 != 0) begin
+            if (pixel_counter%8 == 0) begin
+                scrX = scrX + 1;
+            end
         end else begin
             scrX = 8'b0;
-            scrY = scrY + 1;
+            if (scrY < 6'd63) begin
+                if (line_counter%16 == 0) begin
+                    scrY = scrY + 1;
+                end
+            end else begin
+                scrY = 6'b0;
+            end
+            line_counter = line_counter + 1;
         end
+        pixel_counter = pixel_counter + 1;
         #1
-        $display("caracter = %h", caracter);
+        $display("Char = %h    Line = %d     Pixel = %d     scrX = %d      scrY = %h", char, line_counter, pixel_counter, scrX, scrY);
     end
 endmodule
